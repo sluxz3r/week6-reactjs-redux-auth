479a5d8b80d0797d97b696c0862a2c50903ef261
sluxz3r <rurul161011@gmail.com>
2019-08-08 10:50:12 +0700

    fix

--- src/Publics/redux/actions/member.js
+++ src/Publics/redux/actions/member.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://lib-books-test.herokuapp.com/';
+const url = 'https://lib-books-test.herokuapp.com';
 
 export const getUser = () => {
     return {
