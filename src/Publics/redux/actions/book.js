fdd050922b442f383dae9218d8108d8d9b3bdb8f
sluxz3r <rurul161011@gmail.com>
2019-08-08 10:23:58 +0700

    fix all

--- src/Publics/redux/actions/book.js
+++ src/Publics/redux/actions/book.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const url = 'https://lib-books-test.herokuapp.com/';
+const url = 'https://lib-books-test.herokuapp.com';
 
 export const getBooks = () => {
     return {
